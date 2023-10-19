4ca52428e9bfe1860bbccf14e1b89b50a2ef7925
Zohaib haider <zebihaider123@gmail.com>
2023-10-19 21:16:36 +0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,14 @@ const connectDB = require("./db");
 const express = require('express');
 const app = express();
 require('dotenv').config();
-var cors = require('cors')
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://notia-frontend.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
-app.use(cors())
+
 app.use(express.json())
 const port = 5000;
 connectDB();
