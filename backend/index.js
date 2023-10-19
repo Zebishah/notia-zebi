49ab56204d5fd72186509e420995edf36f23b6cd
Zohaib haider <zebihaider123@gmail.com>
2023-10-19 21:36:08 +0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,7 @@ const connectDB = require("./db");
 const express = require('express');
 const app = express();
 require('dotenv').config();
-var cors = require('cors')
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://notia-frontend.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); // If you need to allow credentials
-    next();
-});
-app.use(cors(
-    {
-        origin: ["https://notia-frontend.vercel.app"],
-        credentials: true
-    }
-))
+
 app.use(express.json())
 const port = 5000;
 connectDB();
