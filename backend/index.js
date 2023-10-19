66a00c43f92342adc46846022177d5ef7aab3b80
Zohaib haider <zebihaider123@gmail.com>
2023-10-19 20:43:24 +0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,16 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 var cors = require('cors')
-var corsOptions = {
-    origin: "https://notia-frontend.vercel.app",
-    Credential: true
-};
-
-
-app.use(cors(corsOptions))
+const allowedOrigins = ['https://notia-frontend.vercel.app'];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 app.use(express.json())
 const port = 5000;
 connectDB();
